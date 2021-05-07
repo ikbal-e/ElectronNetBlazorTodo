14312739c2293f5895f2f3be9f00f69b5b365618
ikbal-e <60185548+ikbal-e@users.noreply.github.com>
2021-05-07 16:34:25 +0300

    Installed LiteDB package

---
+++ TodoBlazorElectron/Entities/ToDo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoBlazorElectron.Entities
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? Deadline { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime? CompletedAt { get; set; }
+        public string Description { get; set; }
+    }
+}
